1fc428ebb2aa14541f7123f2d4c4ca575de50fd9
GDO <2992362310@qq.com>
2023-03-19 21:57:12 +0800

    ....

---
+++ packages/site/src/.umi/core/plugin.ts
@@ -0,0 +1,8 @@
+// @ts-nocheck
+import { Plugin } from 'D:/Code/Code/Demo/demo/node_modules/.pnpm/@umijs+runtime@3.5.38_react@16.14.0/node_modules/@umijs/runtime';
+
+const plugin = new Plugin({
+  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','__mfsu','getInitialState','initialStateConfig','request',],
+});
+
+export { plugin };
---
+++ packages/site/src/.umi/core/routes.ts
@@ -0,0 +1,29 @@
+// @ts-nocheck
+import React from 'react';
+import { ApplyPluginsType } from 'D:/Code/Code/Demo/demo/node_modules/.pnpm/@umijs+runtime@3.5.38_react@16.14.0/node_modules/@umijs/runtime';
+import * as umiExports from './umiExports';
+import { plugin } from './plugin';
+
+export function getRoutes() {
+  const routes = [
+  {
+    "path": "/",
+    "component": require('@/pages/index').default,
+    "exact": true
+  },
+  {
+    "path": "/local",
+    "component": require('@/pages/local').default,
+    "exact": true
+  }
+];
+
+  // allow user to extend routes
+  plugin.applyPlugins({
+    key: 'patchRoutes',
+    type: ApplyPluginsType.event,
+    args: { routes },
+  });
+
+  return routes;
+}
